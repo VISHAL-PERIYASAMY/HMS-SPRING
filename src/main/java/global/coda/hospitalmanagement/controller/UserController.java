07f4a2734c09c7089c84649411e00265092f2cb3
vishalperiyasamy <vishalrv1904@gmail.com>
2020-02-04 14:16:52 +0530

    Hello Service Added

--- src/main/java/global/coda/hospitalmanagement/controller/UserController.java
+++ src/main/java/global/coda/hospitalmanagement/controller/UserController.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,12 @@ public class UserController {
 
 	/** The logger. */
 	private final Logger logger = LogManager.getLogger(UserController.class);
+
+	@GetMapping("/hello")
+	public String getHello()
+	{
+		return "Hello Its Working !!!";
+	}
 
 	@Autowired
 	UserOperationDelegate userOperationDelegate;
